af475c85fe74bee442cbd19271a6533b516afc3e
Gent Rexha <gentr@stud.ntnu.no>
2016-09-07 18:38:24 +0200

    Fixed some stuff, still languange change missing

--- app/src/main/java/com/example/gentr/whatsmyname/MainActivity.java
+++ app/src/main/java/com/example/gentr/whatsmyname/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.gentr.whatsmyname;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.app.Activity;
+import android.content.res.Configuration;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,5 +30,25 @@ public class MainActivity extends AppCompatActivity {
         String input = et.getText().toString();
         myintent.putExtra(extra_input,input);
         startActivity(myintent);
+    }
+    public void langSqButtonOnClick(View v)
+    {
+        Locale locale = new Locale("sq");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_show_name);
+    }
+    public void langEnButtonOnClick(View v)
+    {
+        Locale locale = new Locale("sq");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        this.setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_show_name);
     }
 }
